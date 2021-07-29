b1a114e7249a58169b6b665b8a3a81aa8e081999
vasu.senawongse <vasu@coursesquare.co>
2021-07-30 02:46:31 +0700

    update

--- src/views/pages/Home/Home.vue
+++ src/views/pages/Home/Home.vue
@@ -9,8 +9,87 @@
                 <b-col md="9" cols="12" class="pt-3">
                   <div>
                     <div class="text-center pt-3">
+                      <h5>
+                        เลือกหมวดหมู่ด้านล่าง
+                      </h5>
+                    </div>
+                  </div>
+                </b-col>
+              </b-row>
+              <b-row class="justify-content-center text-center my-4">
+                <b-col md="3" cols="12">
+                  <div>
+                    <i
+                      class="fa fa-map-marker rounded-icon"
+                      :style="
+                        selectedChoice != 0
+                          ? { 'background-color': 'gray' }
+                          : { 'background-color': choices[0].color }
+                      "
+                      @click="selectedChoice = 0"
+                    />
+                  </div>
+                  <div>
+                    แหล่งท่องเที่ยว
+                  </div></b-col
+                >
+                <b-col md="3" cols="12">
+                  <div>
+                    <i
+                      class="fas fa-hotel rounded-icon"
+                      :style="
+                        selectedChoice != 1
+                          ? { 'background-color': 'gray' }
+                          : { 'background-color': choices[1].color }
+                      "
+                      @click="selectedChoice = 1"
+                    />
+                  </div>
+                  <div>
+                    ที่พัก
+                  </div></b-col
+                >
+                <b-col md="3" cols="12">
+                  <div>
+                    <i
+                      class="fas fa-utensils rounded-icon"
+                      :style="
+                        selectedChoice != 2
+                          ? { 'background-color': 'gray' }
+                          : { 'background-color': choices[2].color }
+                      "
+                      @click="selectedChoice = 2"
+                    />
+                  </div>
+                  <div>
+                    ร้านอาหาร
+                  </div></b-col
+                >
+              </b-row>
+            </b-col>
+            <b-col md="4" cols="12" class="form-background">
+              <b-row class="justify-content-center">
+                <b-col md="9" cols="12" class="pt-3">
+                  <div>
+                    <div class="text-center pt-3">
                       <h4>ค้นหาข้อมูลแหล่งท่องเที่ยว</h4>
                     </div>
+
+                    <b-input-group class="my-4">
+                      <b-form-input
+                        alternative
+                        type="text"
+                        placeholder="ค้นหาจากชื่อ"
+                        v-model="location"
+                        style="height : 40px"
+                      >
+                      </b-form-input>
+                      <b-input-group-prepend style="height : 40px;">
+                        <span class="input-group-text" style="width : 40px"
+                          ><i class="fa fa-search"></i
+                        ></span>
+                      </b-input-group-prepend>
+                    </b-input-group>
 
                     <b-input-group class="my-4">
                       <b-form-select
@@ -29,7 +108,7 @@
                       </b-input-group-prepend>
                     </b-input-group>
 
-                    <b-input-group class="my-4">
+                    <b-input-group class="my-4" v-if="selectedChoice == 0">
                       <b-form-select
                         alternative
                         addon-left-icon="ni
@@ -46,22 +125,6 @@
                       </b-input-group-prepend>
                     </b-input-group>
 
-                    <b-input-group>
-                      <b-form-input
-                        alternative
-                        type="text"
-                        placeholder="ชื่อแหล่งท่องเที่ยว"
-                        v-model="location"
-                        style="height : 40px"
-                      >
-                      </b-form-input>
-                      <b-input-group-prepend style="height : 40px;">
-                        <span class="input-group-text" style="width : 40px"
-                          ><i class="fa fa-search"></i
-                        ></span>
-                      </b-input-group-prepend>
-                    </b-input-group>
-
                     <div class="text-center">
                       <b-button
                         class="my-4 w-100"
@@ -74,146 +137,6 @@
                 </b-col>
               </b-row></b-col
             >
-            <b-col md="4" cols="12" class="form-background">
-              <b-row class="justify-content-center">
-                <b-col md="9" cols="12" class="pt-3">
-                  <div>
-                    <div class="text-center pt-3">
-                      <h5>
-                        ค้นหาแหล่งท่องเที่ยวที่สนใจ
-                      </h5>
-                      <h5>
-                        เลือกหมวดหมู่ของแหล่งท่องเที่ยวที่แนะนำด้านล่าง
-                      </h5>
-                    </div>
-                  </div>
-                </b-col>
-              </b-row>
-              <b-row class="justify-content-center text-center my-4">
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fas fa-hotel rounded-icon"
-                      :style="
-                        choices[0].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[0].color }
-                      "
-                      @click="
-                        choices[0].value == false
-                          ? (choices[0].value = true)
-                          : (choices[0].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    ที่พัก
-                  </div></b-col
-                >
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fas fa-utensils rounded-icon"
-                      :style="
-                        choices[1].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[1].color }
-                      "
-                      @click="
-                        choices[1].value == false
-                          ? (choices[1].value = true)
-                          : (choices[1].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    ร้านอาหาร
-                  </div></b-col
-                >
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fa fa-map-marker rounded-icon"
-                      :style="
-                        choices[2].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[2].color }
-                      "
-                      @click="
-                        choices[2].value == false
-                          ? (choices[2].value = true)
-                          : (choices[2].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    แหล่งท่องเที่ยว
-                  </div></b-col
-                >
-              </b-row>
-              <b-row class="justify-content-center text-center">
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fa fa-coffee rounded-icon"
-                      :style="
-                        choices[3].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[3].color }
-                      "
-                      @click="
-                        choices[3].value == false
-                          ? (choices[3].value = true)
-                          : (choices[3].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    คาเฟ่
-                  </div></b-col
-                >
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fa fa-gift rounded-icon"
-                      :style="
-                        choices[4].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[4].color }
-                      "
-                      @click="
-                        choices[4].value == false
-                          ? (choices[4].value = true)
-                          : (choices[4].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    ของฝาก
-                  </div></b-col
-                >
-                <b-col md="3" cols="12">
-                  <div>
-                    <i
-                      class="fas fa-store rounded-icon"
-                      :style="
-                        choices[5].value == false
-                          ? { 'background-color': 'gray' }
-                          : { 'background-color': choices[5].color }
-                      "
-                      @click="
-                        choices[5].value == false
-                          ? (choices[5].value = true)
-                          : (choices[5].value = false)
-                      "
-                    />
-                  </div>
-                  <div>
-                    จุดแวะ
-                  </div></b-col
-                >
-              </b-row>
-            </b-col>
           </b-row>
         </div>
 
@@ -274,14 +197,80 @@
     <div class="hide-desktop">
       <div>
         <div class="home-body p-3">
+          <div class="text-center pt-3">
+            <h4>ค้นหาข้อมูลแหล่งท่องเที่ยว</h4>
+          </div>
+
+          <b-row class="justify-content-center text-center my-4">
+            <b-col md="3" cols="4">
+              <div>
+                <i
+                  class="fa fa-map-marker rounded-icon"
+                  :style="
+                    selectedChoice != 0
+                      ? { 'background-color': 'gray' }
+                      : { 'background-color': choices[0].color }
+                  "
+                  @click="selectedChoice = 0"
+                />
+              </div>
+              <div>
+                แหล่งท่องเที่ยว
+              </div></b-col
+            >
+            <b-col md="3" cols="4">
+              <div>
+                <i
+                  class="fas fa-hotel rounded-icon"
+                  :style="
+                    selectedChoice != 1
+                      ? { 'background-color': 'gray' }
+                      : { 'background-color': choices[1].color }
+                  "
+                  @click="selectedChoice = 1"
+                />
+              </div>
+              <div>
+                ที่พัก
+              </div></b-col
+            >
+            <b-col md="3" cols="4">
+              <div>
+                <i
+                  class="fas fa-utensils rounded-icon"
+                  :style="
+                    selectedChoice != 2
+                      ? { 'background-color': 'gray' }
+                      : { 'background-color': choices[2].color }
+                  "
+                  @click="selectedChoice = 2"
+                />
+              </div>
+              <div>
+                ร้านอาหาร
+              </div></b-col
+            >
+          </b-row>
           <b-row class="justify-content-center">
             <b-col md="4" cols="12">
               <b-row class="justify-content-center">
                 <b-col md="9" cols="12" class="pt-3">
                   <div>
-                    <div class="text-center pt-3">
-                      <h4>ค้นหาข้อมูลแหล่งท่องเที่ยว</h4>
-                    </div>
+                    <b-input-group class="my-4">
+                      <b-form-input
+                        alternative
+                        type="text"
+                        placeholder="ค้นหาจากชื่อ"
+                        v-model="location"
+                        style="height : 40px"
+                      >
+                      </b-form-input>
+                      <b-input-group-prepend style="height : 40px;">
+                        <span class="input-group-text" style="width : 40px"
+                          ><i class="fa fa-search"></i
+                        ></span>
+                      </b-input-group-prepend>
+                    </b-input-group>
 
                     <b-input-group class="my-4">
                       <b-form-select
@@ -300,7 +289,7 @@
                       </b-input-group-prepend>
                     </b-input-group>
 
-                    <b-input-group class="my-4">
+                    <b-input-group class="my-4" v-if="selectedChoice == 0">
                       <b-form-select
                         alternative
                         addon-left-icon="ni
@@ -316,150 +305,11 @@
                         ></span>
                       </b-input-group-prepend>
                     </b-input-group>
-
-                    <b-input-group>
-                      <b-form-input
-                        alternative
-                        type="text"
-                        placeholder="ชื่อแหล่งท่องเที่ยว"
-                        v-model="location"
-                        style="height : 40px"
-                      >
-                      </b-form-input>
-                      <b-input-group-prepend style="height : 40px;">
-                        <span class="input-group-text" style="width : 40px"
-                          ><i class="fa fa-search"></i
-                        ></span>
-                      </b-input-group-prepend>
-                    </b-input-group>
                   </div>
                 </b-col> </b-row
             ></b-col>
           </b-row>
-          <b-row class="justify-content-center text-center my-4">
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fas fa-hotel rounded-icon"
-                  :style="
-                    choices[0].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[0].color }
-                  "
-                  @click="
-                    choices[0].value == false
-                      ? (choices[0].value = true)
-                      : (choices[0].value = false)
-                  "
-                />
-              </div>
-              <div>
-                ที่พัก
-              </div></b-col
-            >
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fas fa-utensils rounded-icon"
-                  :style="
-                    choices[1].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[1].color }
-                  "
-                  @click="
-                    choices[1].value == false
-                      ? (choices[1].value = true)
-                      : (choices[1].value = false)
-                  "
-                />
-              </div>
-              <div>
-                ร้านอาหาร
-              </div></b-col
-            >
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fa fa-map-marker rounded-icon"
-                  :style="
-                    choices[2].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[2].color }
-                  "
-                  @click="
-                    choices[2].value == false
-                      ? (choices[2].value = true)
-                      : (choices[2].value = false)
-                  "
-                />
-              </div>
-              <div>
-                แหล่งท่องเที่ยว
-              </div></b-col
-            >
-          </b-row>
-          <b-row class="justify-content-center text-center">
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fa fa-coffee rounded-icon"
-                  :style="
-                    choices[3].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[3].color }
-                  "
-                  @click="
-                    choices[3].value == false
-                      ? (choices[3].value = true)
-                      : (choices[3].value = false)
-                  "
-                />
-              </div>
-              <div>
-                คาเฟ่
-              </div></b-col
-            >
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fa fa-gift rounded-icon"
-                  :style="
-                    choices[4].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[4].color }
-                  "
-                  @click="
-                    choices[4].value == false
-                      ? (choices[4].value = true)
-                      : (choices[4].value = false)
-                  "
-                />
-              </div>
-              <div>
-                ของฝาก
-              </div></b-col
-            >
-            <b-col md="3" cols="4">
-              <div>
-                <i
-                  class="fas fa-store rounded-icon"
-                  :style="
-                    choices[5].value == false
-                      ? { 'background-color': 'gray' }
-                      : { 'background-color': choices[5].color }
-                  "
-                  @click="
-                    choices[5].value == false
-                      ? (choices[5].value = true)
-                      : (choices[5].value = false)
-                  "
-                />
-              </div>
-              <div>
-                จุดแวะ
-              </div></b-col
-            >
-          </b-row>
+
           <div class="text-center">
             <b-button class="my-4 w-100" variant="info" @click="search()"
               >ค้นหา</b-button
@@ -558,14 +408,8 @@ export default {
         'วัฒนธรรม',
         'วิทยาศาสตร์',
       ],
-      choices: [
-        { color: 'pink', value: true },
-        { color: 'violet', value: true },
-        { color: 'orange', value: true },
-        { color: '#BDA55D', value: true },
-        { color: '#FFD300', value: true },
-        { color: '#3DED97', value: true },
-      ],
+      selectedChoice: 0,
+      choices: [{ color: 'orange' }, { color: 'green' }, { color: 'pink' }],
       result: [],
       isSearch: false,
     };
