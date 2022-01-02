f0bcd153e8f4b4f5a21cd0b940774202937d0c91
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-02 17:35:19 +0700

    Update Document.vue

--- src/views/pages/Documents/Document.vue
+++ src/views/pages/Documents/Document.vue
@@ -2,7 +2,7 @@
   <div class="home-body">
     <iframe
       style="width:100%;height:800px"
-      src="https://pubhtml5.com/bookcase/bsrn/gold"
+      src="https://pubhtml5.com/bookcase/repy/gold"
       seamless="seamless"
       scrolling="no"
       frameborder="0"
