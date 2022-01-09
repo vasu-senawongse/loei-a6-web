b7f738895946d5857714e3d71a90c5fee7494f5e
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-09 13:30:43 +0700

    update

--- src/views/pages/Usage/Usage.vue
+++ src/views/pages/Usage/Usage.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="home-body">
-    <b-container class="my-5">
+    <b-container>
       <div class="text-center">
-        <img
-          src="../../../assets/images/qrcode.jpg"
-          width="200"
-          height="100%"
-        />
+        <b-row>
+          <b-col md="12" cols="12">
+            <h2>คู่มือการใช้งานระบบ</h2>
+            <div>
+              <iframe
+                class="usage-iframe"
+                src="https://online.pubhtml5.com/qyqi/tnyi/"
+                seamless="seamless"
+                scrolling="no"
+                frameborder="0"
+                allowtransparency="true"
+                allowfullscreen="true"
+              ></iframe>
+            </div>
+          </b-col>
+          <b-col md="12" cols="12">
+            <h2>วิดีโอการใช้งานระบบ</h2>
+            <div>
+              <iframe
+                sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+                class="usage-iframe"
+                src="https://drive.google.com/file/d/1hJ2vANmAsz38oISfOhBtMLQzJgJrq1R9/preview"
+                frameborder="0"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </b-col>
+        </b-row>
       </div>
     </b-container>
   </div>
